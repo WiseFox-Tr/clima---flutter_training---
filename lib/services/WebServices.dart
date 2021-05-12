a284c0e17ce4e1be5f935f12761ffccc4100ad26
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-12 21:57:14 +0200

    add units : metric for temperature & return a Weather object

--- lib/services/WebServices.dart
+++ lib/services/WebServices.dart
@@ -1,18 +1,22 @@
+import 'package:clima/model/Weather.dart';
 import 'package:clima/utilities/HttpRequest.dart';
 import 'package:clima/utilities/APIWeather.dart';
 
 class WebServices {
 
-  static Future<dynamic> getWeatherDataByCurrentLocation(String latitude, String longitude) async {
+  static Future<Weather> getWeatherDataByCurrentLocation(String latitude, String longitude) async {
+    dynamic data;
     Uri url = Uri.https(
       ApiWeather.authority,
       ApiWeather.unencodedPath,
       {
         'lat': latitude,
         'lon': longitude,
+        'units': 'metric',
         'appid': ApiWeather.appid
       },
     );
-    return HttpRequest.httpRequestGet(url);
+    data = await HttpRequest.httpRequestGet(url);
+    return Weather(data['weather'][0]['id'], data['name'], data['main']['temp'].toInt());
   }
 }
