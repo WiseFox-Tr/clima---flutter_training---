49c5cb3add0a460804b0ccf632ec0271b57aeefb
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-12 15:51:48 +0200

    rename some methods & add a push to location screen when user want to get weather for the current location

--- lib/controller/ClimaController.dart
+++ lib/controller/ClimaController.dart
@@ -1,24 +1,35 @@
 
+import 'package:clima/screens/location_screen.dart';
 import 'package:clima/services/LocationService.dart';
 import 'package:clima/services/WebServices.dart';
+import 'package:flutter/material.dart';
 
 class ClimaController {
 
   LocationService _location = LocationService();
   var data;
 
-  void getWeatherData() async {
+  void getWeatherDataByCurrentLocation(BuildContext context) async {
     try{
-      await _getLocation();
-      data = await WebServices.getDataByCurrentLocation(_location.latitude.toString(), _location.longitude.toString());
+      await _getCurrentLocation();
+      data = await WebServices.getWeatherDataByCurrentLocation(_location.latitude.toString(), _location.longitude.toString());
       print('data : $data');
+      pushToScreen(context, LocationScreen());
     } catch(e) {
       print('exception : $e');
     }
   }
 
-  Future<void> _getLocation() async {
+  Future<void> _getCurrentLocation() async {
     await _location.getCurrentLocation();
     print("Current latitude : ${_location.latitude}\nCurrent longitude : ${_location.longitude}");
+  }
+
+  ///Uses Navigator.push.
+  void pushToScreen(BuildContext context, Widget destinationScreen) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => destinationScreen),
+    );
   }
 }
