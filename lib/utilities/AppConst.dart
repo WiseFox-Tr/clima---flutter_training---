67db319e5d6eff82f302074619e97860982d9309
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-13 18:22:11 +0200

    add a textfield decoration const

--- lib/utilities/AppConst.dart
+++ lib/utilities/AppConst.dart
@@ -26,6 +26,18 @@ class AppConst {
   static const conditionTextStyle = TextStyle(
     fontSize: 100.0,
   );
+
+  static const textFieldDecoration = InputDecoration(
+    filled: true,
+    fillColor: Colors.white,
+    icon: Icon(Icons.location_city, color: Colors.white,),
+    hintText: 'enter city name',
+    hintStyle: TextStyle(color: Colors.grey),
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+      borderSide: BorderSide.none,
+    ),
+  );
 }
 
 
