7faad266bed664789ce23a28088931a82a368e1c
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-14 16:21:20 +0200

    add comment

--- lib/services/APIWeatherConst.dart
+++ lib/services/APIWeatherConst.dart
@@ -3,5 +3,5 @@ import 'package:clima/services/api_key.dart';
 class ApiWeatherConst {
   static const String authority = 'api.openweathermap.org';
   static const String unencodedPath = '/data/2.5/weather';
-  static const String appid = apiKey;
+  static const String appid = apiKey; //value come from a file not followed by git
 }
