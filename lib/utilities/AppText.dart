6ac668009f2a09100725176221e0c4f4602d55cd
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-14 16:11:54 +0200

    add a class for texts to display

---
+++ lib/utilities/AppText.dart
@@ -0,0 +1,14 @@
+class AppTexts {
+  //Weather Model strings
+  static const String hotWeather = 'It\'s 🍦 time';
+  static const String niceWeather = 'Time for shorts and 👕';
+  static const String coldWeather = 'You\'ll need 🧣 and 🧤';
+  static const String mediumWeather = 'Bring a 🧥 just in case';
+
+  //Controller strings
+  static const String defaultErrorMessage = 'Oups ! A problem occurs...';
+
+  //screens strings
+  static const String labelInputLocation = 'Weather for input location';
+  static const String labelCurrentLocation = 'Weather for your location';
+}
