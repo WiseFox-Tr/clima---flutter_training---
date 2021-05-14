cac9bbe96c5a96c54226484f88e1221102fd90f6
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-14 16:15:48 +0200

    check now if request has a 200 status code but data are empty & throw exception

--- lib/utilities/HttpRequest.dart
+++ lib/utilities/HttpRequest.dart
@@ -6,16 +6,16 @@ class HttpRequest {
 
   static dynamic httpRequestGet(Uri url) async {
     http.Response response = await http.get(url);
+    var data;
 
     if(response.statusCode == 200) {
-      var data = response.body;
-      print('status code : ${response.statusCode}');
-      print('data retrieved : $data');
+      data = response.body;
+      print('status code : ${response.statusCode} -> data retrieved : $data');
       return jsonDecode(data);
-    }
-    else {
-      print('response code : ${response.statusCode}');
-      throw Exception('failed to retrieve data : response code : ${response.statusCode}') ;
+    } else if (data == null) {
+      throw Exception('data retrieved are empty');
+    } else {
+      throw Exception('failed to retrieve data : response code : ${response.statusCode}');
     }
   }
 }
