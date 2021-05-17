019d73212e896b8d34daab74561834d2fa1eb90d
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-17 13:53:10 +0200

    update http - returns data only if response code == 200 & if data is not empty

--- lib/services/HttpRequest.dart
+++ lib/services/HttpRequest.dart
@@ -6,14 +6,11 @@ class HttpRequest {
 
   static dynamic httpRequestGet(Uri url) async {
     http.Response response = await http.get(url);
-    var data;
+    var data = response.body;
 
-    if(response.statusCode == 200) {
-      data = response.body;
+    if(response.statusCode == 200 && data != null) {
       print('status code : ${response.statusCode} -> data retrieved : $data');
       return jsonDecode(data);
-    } else if (data == null) {
-      throw Exception('data retrieved are empty');
     } else {
       throw Exception('failed to retrieve data : response code : ${response.statusCode}');
     }
